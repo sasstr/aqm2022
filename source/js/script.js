eccf15b5ad89d3864c2cc0749beeed8ffc693190
Alexander Strakhovenko <sasstr@gmail.com>
2022-09-04 14:56:02 +0300

    Добавляет JS закрыть меню по нажатию на Esc

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,14 @@ const btnHeaderMenu = document.querySelector('.header__toggle');
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerMenuClose = document.querySelector('.burger-menu-close')
 
+const onKeyupEsc = function (evt) {
+  if (evt.keyCode == 27){
+    navigation.classList.add('hidden');
+    burgerMenuClose.classList.add('hidden');
+    burgerMenu.classList.remove('hidden');
+  }
+}
+
 const onClickBtnHeaderMenu = function (evt) {
   evt.preventDefalt;
   navigation.classList.toggle('hidden');
@@ -15,4 +23,5 @@ const onClickBtnHeaderMenu = function (evt) {
   }
 };
 
+document.addEventListener('keyup', onKeyupEsc);
 btnHeaderMenu.addEventListener('click', onClickBtnHeaderMenu);
