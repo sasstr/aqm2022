4dfee200c14c85775d50860aa2694a0ed5ce378f
unknown <c:\work\mail>
2022-09-04 17:29:38 +0300

    animation fade

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,8 @@
+
+var jQueryScript = document.createElement('script');
+jQueryScript.setAttribute('src', 'https://code.jquery.com/jquery-3.6.1.min.js');
+document.head.appendChild(jQueryScript);
+
 const navigation = document.querySelector('.navigation');
 const btnHeaderMenu = document.querySelector('.header__toggle');
 const burgerMenu = document.querySelector('.burger-menu');
@@ -25,3 +30,34 @@ const onClickBtnHeaderMenu = function (evt) {
 
 document.addEventListener('keyup', onKeyupEsc);
 btnHeaderMenu.addEventListener('click', onClickBtnHeaderMenu);
+
+function defer(){
+   if ( window.jQuery){
+
+      $("section, .footer, .main__container").css('opacity','0');
+      $(window).scroll(function() {OnWinScroll()});
+      OnWinScroll()
+
+
+   }
+   else{
+      window.setTimeout("defer();",100);
+   }
+}
+defer();
+
+function OnWinScroll()
+{
+ $("section, .footer, .main__container").each(function(){if(onmyscreen($(this))) $(this).animate({opacity: 1}, 1000);;})
+}
+
+function onmyscreen(el)
+{
+    if(el.css("opacity")!=0) return false;
+    var window_top = $(window).scrollTop();
+    var offset = el.offset().top;
+    if (offset <= window_top+$(window).height())  return true;
+
+    return false;
+
+}
