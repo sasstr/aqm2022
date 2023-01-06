1c65eb5c43404dc669d1d9ce1e8a736eb084b9c6
Alexander Strakhovenko <sasstr@gmail.com>
2023-01-07 01:54:31 +0300

    Добавляет билд

--- docs/js/script.min.js
+++ docs/js/script.min.js
@@ -9,26 +9,25 @@ const burgerMenu = document.querySelector('.burger-menu');
 const burgerMenuClose = document.querySelector('.burger-menu-close');
 
 // переключение списка работ по наведению мышки прозрачные/не прозрачные
-if(document.querySelector('.work__list') && document.querySelectorAll('.work__item') ){
-    const workList = document.querySelector('.work__list');
-    const workItems = workList.querySelectorAll('.work__item');
+// if(document.querySelector('.work__list') && document.querySelectorAll('.work__item') ){
+//     const workList = document.querySelector('.work__list');
+//     const workItems = workList.querySelectorAll('.work__item');
 
-    const isMouseOnElement= function (e) {
-        if(e.target.classList.contains('work__item')) {
-            workItems.forEach(it => it.style.opacity = 0.5)
-            e.target.style.opacity = 1;
-        }
-    };
+//     const isMouseOnElement= function (e) {
+//         if(e.target.classList.contains('work__item')) {
+//             workItems.forEach(it => it.style.opacity = 0.5)
+//             e.target.style.opacity = 1;
+//         }
+//     };
 
-    const isMouseOutOnElement = function () {
-        workItems.forEach(it => it.style.opacity = 1)
-    };
+//     const isMouseOutOnElement = function () {
+//         workItems.forEach(it => it.style.opacity = 1)
+//     };
 
-    workList.addEventListener('mouseover', isMouseOnElement);
+//     workList.addEventListener('mouseover', isMouseOnElement);
 
-    workList.addEventListener('mouseout', isMouseOutOnElement);
-}
-
+//     workList.addEventListener('mouseout', isMouseOutOnElement);
+// }
 
 const onKeyupEsc = function (evt) {
     if (evt.keyCode == 27){
@@ -41,7 +40,7 @@ const onKeyupEsc = function (evt) {
 }
 // Открыетие и закрытие меню гамбургер
 const onClickBtnHeaderMenu = function (evt) {
-    evt.preventDefalt;
+    evt.preventDefault;
     navigation.classList.toggle('hidden');
     if(navigation.classList.contains('hidden')) {
         burgerMenuClose.classList.add('hidden');
@@ -49,14 +48,14 @@ const onClickBtnHeaderMenu = function (evt) {
         enableScrolling();
         $(".header").css("background-color","");
         burgerMenu.classList.remove('hidden');
-        document.querySelector('.header__logo').classList.remove('hidden');
+        // document.querySelector('.header__logo').classList.remove('hidden');
     } else {
         burgerMenu.classList.add('hidden');
         burgerMenuClose.classList.remove('hidden');
         $('body').bind('touchmove', function(e){e.preventDefault()})
         disableScrolling();
         $(".header").css("background-color","#003137");
-        document.querySelector('.header__logo').classList.add('hidden');
+        // document.querySelector('.header__logo').classList.add('hidden');
     }
 };
 
@@ -84,12 +83,12 @@ function defer(method){
     else{window.setTimeout("defer("+method+");",500);}
 }
 
-
 function OnWinScroll()
 {
     $("section, .footer,  .projects__card, .projects__title").not('main__wrapper.projects').each(function(){if(onmyscreen($(this)))
                 MyShow($(this));})
 }
+
 function MyShow(el)
 {
     el.find("> *").css("opacity",0);
@@ -146,12 +145,9 @@ function PreFormatAnimation()
 
         }
     );
-
-
-
 }
-function animate1digit(element, to, speed)
-{
+
+function animate1digit(element, to, speed) {
     if(to>0)
     $(element).children().first().css('margin-top',-$(element).height()+'px');
     if(to<10)
@@ -161,6 +157,7 @@ function animate1digit(element, to, speed)
     $(element).children().first().animate({'margin-top':-$(element).height()*10},{speed:speed*10,complete:function(){animate1digit(element,to-10,speed)}});
     }
 }
+
 function animate1number(element,speed)
 {
     $(element).children().each(function(index,value)
@@ -173,14 +170,14 @@ function animate1number(element,speed)
       total=parseInt(total/(10**(2-index)));
       if(total>40) total=40+total%10;
       if(total>10) speed=300/(total%10); else speed=300;
-      console.log(total)      ;
-      console.log(speed)      ;
+      //console.log(total)      ;
+      //console.log(speed)      ;
       animate1digit(value,total,speed);
-
     })
     //animate1digit($(element).children().last(),$(element).attr("data-animation")%100,speed);
 
 }
+
 function animateALLnumbers()
 {
     $(".achievement__number").each(function(){animate1number($(this))});;
